fdccbf81f2261fed3478732a509e428f86f97ea8
Anuj <anujchourasiyadon@gmail.com>
2020-07-04 17:37:59 +0530

    Revert "modified comments in settings.py to work with heroku"
    This reverts commit 04c3a728ef6e896940d1652e0eb9fd4dad2aa6f5.

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -77,13 +77,16 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url 
+    DATABASES = { 'default': dj_database_url.config()}
+else:
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
+    }
 
 
 # Password validation
@@ -124,10 +127,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS=(os.path.join(BASE_DIR,"static"),)
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-#  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-import dj_database_url 
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+#import whitenoise
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
