96b4be2001aee7e72488d64fec1df4f8a37ff24b
Anuj <anujchourasiyadon@gmail.com>
2020-07-04 17:57:46 +0530

    adding db dile for myadmin

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -78,12 +78,16 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url 
+    DATABASES = { 'default': dj_database_url.config()}
+else:
+    DATABASES = {
+        'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+                   }
+                }
 
 
 # Password validation
