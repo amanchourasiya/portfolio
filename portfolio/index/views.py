20a823eac34e2b6364e1c7e1ba1dbeddc1ee3c75
Anuj <anujchourasiyadon@gmail.com>
2020-07-07 19:37:17 +0530

    passing json file to home page blogs

--- portfolio/index/views.py
+++ portfolio/index/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+import json
+import os
 
 def index(request):
-    return render(request, 'index.html', {})
+    if os.path.exists('static/blog-cards.json'):
+        with open("static/blog-cards.json","r") as fileobj:
+            data=json.load(fileobj)
+        return render(request, 'index.html',{'data':data})
+
+    else:
+        
+         return render(request, 'index.html',{'data':{}})
