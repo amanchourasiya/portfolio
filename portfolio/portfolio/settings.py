758e15d6f78ba3a4ca262e0c6c03e0ab5ace1279
Aman Chourasiya <amanchourasia6@gmail.com>
2020-07-09 22:10:21 +0530

    Fixing static dir

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS=(os.path.join(BASE_DIR,"static"),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 import whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
