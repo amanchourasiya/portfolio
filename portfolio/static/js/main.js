d5848ce33a847409097f5ed02ae92754da71f8eb
aman chourasiya <amanchourasia6@gmail.com>
2020-07-25 19:56:10 +0530

    Added api endpoint for blog claps

--- portfolio/static/js/main.js
+++ portfolio/static/js/main.js
@@ -14,48 +14,40 @@ const responsive = {
 }
 $(document).ready(function () {
 	$(".owl-carousel").owlCarousel({
-		loop:true,
-		autoplay:true,
-		autoplayTimeout:2500,
-		responsive:responsive,
-		});
-		checkBlogViews();	
-		//updateMetaTags();
+		loop: true,
+		autoplay: true,
+		autoplayTimeout: 2500,
+		responsive: responsive,
+	});
+	checkBlogViews();
 });
 
-function checkBlogViews(){
+function checkBlogViews() {
 	let url = location.href.split("/").slice(-3);
-	if (url[0] == "blog"){
+	if (url[0] == "blog") {
 		getPageViews(url[1]);
 		incrementPageViews(url[1]);
 	}
 };
 
-function getPageViews(blogName){   
-	let pageViews; 
-    $.get('/api/v1/getviews/'+ blogName , function(data, status){
+function getPageViews(blogName) {
+	let pageViews;
+	let pageClaps;
+	$.get('/api/v1/getviews/' + blogName, function (data, status) {
 		console.log(data);
 		pageViews = data["blog_views"];
+		pageClaps = data["blog_claps"];
 		console.log(pageViews);
+		console.log(pageClaps);
 		$('#page-views').html(pageViews);
+		// set page claps attribute
 	});
-	// Change page views
-	//window.alert(pageViews);
-	
 };
 
-function incrementPageViews(blogName){
-	$.post('/api/v1/incrementviews', {'blog_name': blogName});
+function incrementPageViews(blogName) {
+	$.post('/api/v1/incrementviews', { 'blog_name': blogName });
 };
 
-function updateMetaTags(){
-	// Updating og tags
-	$('meta[property="og:title"]').attr('content' ,$('#blog-title').text());
-	$('meta[property="og:description"]').attr('content', $('#blog-description').text());
-	$('meta[property="og:url"]').attr('content', window.location.href);
-
-	// updating twitter tags
-	$('meta[name="twitter:title"]').attr('content', $('#blog-title').text());
-	$('meta[name="twitter:description"]').attr('content' ,$('#blog-description').text());
-	$('meta[name="twitter:url"]').attr('content',window.location.href);
+function incrementClaps(blogName) {
+	$.post('/api/v1/incrementclaps', { 'blog_name': blogName });
 };
