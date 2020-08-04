25b6a1a6e2cebf7ef4f8c5a932112df25fb684c8
Aman Chourasiya <amanchourasia6@gmail.com>
2020-08-04 11:39:56 +0530

    Registering models with django admin

--- portfolio/apiserver/admin.py
+++ portfolio/apiserver/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.apps import apps
+
+
+models = apps.get_models()
+
+for model in models:
+    admin.site.register(model)
