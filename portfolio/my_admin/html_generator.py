f6d2177b423adaa16b5d9c0b721510056cad0d0b
Aman Chourasiya <amanchourasia6@gmail.com>
2020-08-26 14:06:40 +0530

    Update html_generator.py

--- portfolio/my_admin/html_generator.py
+++ portfolio/my_admin/html_generator.py
@@ -260,9 +260,8 @@ def create_html(title, blocks, path='blog/templates/blog/'):
 </footer>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="{%static 'js/owl.carousel.min.js'%}"></script>
 <script src="/static/js/blog.js"</script>
-
+<script src="{%static 'js/owl.carousel.min.js'%}"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 </html>
 
